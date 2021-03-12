2754e5ac8d01f59303b92c68b20259793e129449
zhouchangquan <changquan.zhou@163.com>
2021-03-12 10:47:12 +0800

    use the Validatetor librayr in MainActivity

--- app/src/main/java/com/raywenderlich/android/validatetorproject/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/validatetorproject/MainActivity.kt
@@ -25,14 +25,20 @@ package com.raywenderlich.android.validatetorproject
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.EditText
+import android.widget.Toast
+import com.raywenderlich.android.validatetor.ValidateTor
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var validateTor:ValidateTor
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setUpUiWidgets()
+
+    //initialize validatetor instance
+    validateTor = ValidateTor()
   }
 
   private fun setUpUiWidgets() {
@@ -45,16 +51,48 @@ class MainActivity : AppCompatActivity() {
 
   private fun validateCreditCardField(editText: EditText) {
     val str = editText.text.toString()
-    // TODO: Validate credit card number field, not implemented
+
+    if(validateTor.isEmpty(str)){
+      editText.error = "Field is empty!"
+    }
+
+    else if(!validateTor.validateCreditCard(str)){
+      editText.error = "Invalid Credit Card number!"
+    }
+    else{
+      Toast.makeText(this,"Valid Credit Card Number!",Toast.LENGTH_SHORT).show()
+    }
   }
 
   private fun validatePasswordField(editText: EditText) {
     val str = editText.text.toString()
-    // TODO: Validate password field, not implemented
+    if (validateTor.isEmpty(str)) {
+      editText.error = "Field is empty!"
+    }
+
+    else if (validateTor.isAtleastLength(str, 8)
+        && validateTor.hasAtleastOneDigit(str)
+        && validateTor.hasAtleastOneUppercaseCharacter(str)
+        && validateTor.hasAtleastOneSpecialCharacter(str)) {
+      Toast.makeText(this, "Valid Password!", Toast.LENGTH_SHORT).show()
+    } else {
+      editText.error = "Password needs to be a minimum length of 8 characters and should have at least 1 digit, 1 upppercase letter and 1 special character "
+    }
+
   }
 
   private fun validateEmailField(editText: EditText) {
     val str = editText.text.toString()
-    // TODO: Validate email field, not implemented
+
+    if (validateTor.isEmpty(str)) {
+      editText.error = "Field is empty!"
+    }
+
+    else if (!validateTor.isEmail(str)) {
+      editText.error = "Invalid Email"
+    } else {
+      Toast.makeText(this, "Valid Email!", Toast.LENGTH_SHORT).show()
+    }
+
   }
 }
